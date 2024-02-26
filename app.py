4e386ac3b00db370f22b7b29b080f9f4aaf84540
ANNAPAVAN <pavananna006@gmail.com>
2024-02-26 17:49:11 +0530

    movies

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+def recommand(movie):
+  movie_index = movies[movies["title"] == movie].index[0]
+  distances = similarity[movie_index] 
+  movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+
+  recommended_movies = []
+  for i in movies_list:
+        
+    recommended_movies.append(movies.iloc[i[0]].title)
+
+  return recommended_movies
+
+
+
+movies_dict = pickle.load(open("../Files/movies_dict.pkl", "rb"))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open("../Files/similarity.pkl", "rb"))
+
+
+
+st.title("Movie Recommander System")
+
+selected_movie_name = st.selectbox(
+    "Type movie related",
+    movies["title"].values
+)
+
+if st.button("Recommand"):
+    recommandations = recommand(selected_movie_name)
+    for i in recommandations:
+      st.write(i)
+
+
+
+
+
+
+
